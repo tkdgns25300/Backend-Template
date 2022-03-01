390e1ac5fb90d5d3f24809c7573b2a70cf2e70f5
unknown <tkdgns25300@gmail.com>
2022-03-01 23:04:16 +0900

    setup 1

---
+++ app.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.use('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}...`);
+})
